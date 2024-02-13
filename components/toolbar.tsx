c6473d87e7234af3c3d3fc8f7b609712afac6739
KaikySantos <carlos.kaiky62@gmail.com>
2024-02-13 16:08:14 -0300

    feat: cover image upload

--- components/toolbar.tsx
+++ components/toolbar.tsx
@@ -8,6 +8,7 @@ import React, { ElementRef, useRef, useState } from "react"
 import TextAreaAutosize from "react-textarea-autosize"
 import { useMutation } from "convex/react"
 import { api } from "@/convex/_generated/api"
+import { useCoverImage } from "@/hooks/use-cover-image"
 
 interface ToolbarProps {
   initialData: Doc<"documents">
@@ -24,6 +25,8 @@ export const Toolbar = ({
 
   const update = useMutation(api.documents.update)
   const removeIcon = useMutation(api.documents.removeIcon)
+
+  const coverImage = useCoverImage()
   
   const enableInput = () => {
     if (preview) return
@@ -106,7 +109,7 @@ export const Toolbar = ({
         )}
         {!initialData.coverImage && !preview && (
           <Button
-            onClick={() => {}}
+            onClick={coverImage.onOpen}
             className="text-muted-foreground text-xs"
             variant="outline"
             size="sm"
