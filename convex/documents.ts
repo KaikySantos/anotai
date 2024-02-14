471338b9eef63cff858dfbe6fd96d4de1355273a
KaikySantos <carlos.kaiky62@gmail.com>
2024-02-14 15:54:13 -0300

    feat: remove image

--- convex/documents.ts
+++ convex/documents.ts
@@ -350,3 +350,32 @@ export const removeIcon = mutation({
     return document
   }
 })
+
+export const removeCoverImage = mutation({
+  args: { id: v.id("documents") },
+  handler: async (ctx, args) => {
+    const identify = await ctx.auth.getUserIdentity()
+
+    if (!identify) {
+      throw new Error("Not authenticated")
+    }
+
+    const userId = identify.subject
+
+    const existingDocument = await ctx.db.get(args.id)
+
+    if (!existingDocument) {
+      throw new Error("Not found")
+    }
+
+    if (existingDocument.userId !== userId) {
+      throw new Error("Unauthorized")
+    }
+
+    const document = await ctx.db.patch(args.id, {
+      coverImage: undefined
+    })
+
+    return document
+  }
+})
